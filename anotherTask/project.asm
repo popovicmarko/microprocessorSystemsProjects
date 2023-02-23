72a7cda93d42bef0627607034e250b0d1ab9ecc0
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2023-02-23 14:00:52 +0100

    finish

--- anotherTask/project.asm
+++ anotherTask/project.asm
@@ -1,47 +1,47 @@
-.MODEL SMALL
-.STACK 100h
-.DATA
-    array db 3, 8, 2, 5, 12, 16, 44, 2, 9, 100
-    
-.CODE
-    MAIN PROC
-        MOV AX, @DATA
-        MOV DS, AX
-        
-        MOV CX, 0
-        MOV CL, 10
+.MODEL SMALL  ; definisanje modela memorije
+.STACK 100h   ; definisanje velicine steka
+.DATA         ; definisanje segmenta podataka
+    array db 3, 8, 2, 5, 12, 16, 44, 2, 9, 100  ; deklarisanje niza za sortiranje
 
-        LEA SI, array
+.CODE         ; definisanje segmenta koda
+    MAIN PROC  ; pocetak glavnog programa
+        MOV AX, @DATA  ; ucitavanje adrese segmenta podataka u registar AX
+        MOV DS, AX     ; ucitavanje adrese segmenta podataka u registar DS
+        
+        MOV CX, 0      ; postavljanje CX na 0
+        MOV CL, 10     ; postavljanje CX na 10 (duzina niza)
+
+        LEA SI, array  ; ucitavanje adrese niza u registar SI
 
         
-        MOV BX, 0
+        MOV BX, 0      ; postavljanje BX na 0
         
-     BUBBLE_SORT:
-        dec CX
-        LEA SI, array
-        CMP CX, SI
-        JLE EXIT
+     BUBBLE_SORT:     ; oznaka za pocetak petlje sortiranja
+        dec CX        ; smanjenje brojaca petlje za 1
+        LEA SI, array  ; ucitavanje adrese niza u registar SI
+        CMP CX, SI     ; uporedjivanje brojaca petlje sa adresom niza u SI
+        JLE EXIT       ; ako je brojac petlje manji ili jednak adresi niza, izlazak iz petlje
         
-        SWAP:
-        CMP SI, CX
-        JGE BUBBLE_SORT
+        SWAP:          ; oznaka za pocetak zamene elemenata
+        CMP SI, CX     ; uporedjivanje adresa dva elementa
+        JGE BUBBLE_SORT ; ako su adrese dva elementa jednake, vracanje na pocetak petlje
         
-        MOV BL, array[SI]
-        MOV BH, array[SI+1]
-        CMP BL, BH
-        JLE INCREMENT
+        MOV BL, array[SI]      ; ucitavanje prvog elementa u registar BL
+        MOV BH, array[SI+1]    ; ucitavanje drugog elementa u registar BH
+        CMP BL, BH             ; uporedjivanje dva elementa
+        JLE INCREMENT         ; ako je prvi manji ili jednak od drugog, skok na INCREMENT
         
-        MOV array[SI], BH
-        MOV array[SI+1], BL
-        INC SI
+        MOV array[SI], BH      ; smestanje drugog elementa na adresu prvog elementa
+        MOV array[SI+1], BL    ; smestanje prvog elementa na adresu drugog elementa
+        INC SI                 ; inkrementiranje registra SI (adrese niza)
         
-        JMP SWAP
+        JMP SWAP               ; skok na pocetak zamene
         
+     INCREMENT:               ; oznaka za inkrementiranje adrese niza
+        INC SI                 ; inkrementiranje registra SI (adrese niza)
+        JMP SWAP               ; skok na pocetak zamene
         
-     INCREMENT:
-        INC SI
-        JMP SWAP
-     EXIT:
-     LEA SI, array
+     EXIT:                    ; oznaka za kraj petlje
+     LEA SI, array            ; ucitavanje adrese niza u registar SI
 
-     END
+     END                      ; kraj programa
