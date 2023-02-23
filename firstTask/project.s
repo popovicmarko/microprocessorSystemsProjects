bd58173148e8fb84a001f2e5d02504ed548c384b
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2023-02-23 13:52:39 +0100

    finish

--- firstTask/project.s
+++ firstTask/project.s
@@ -1,61 +1,61 @@
 .data
-  matrix:
-    .word 1 2
-    .word 3 4
+  matrix:       # deklaracija matrice
+    .word 1 2   # vrednosti za prvi red
+    .word 3 4   # vrednosti za drugi red
 
-  rows:
+  rows:         # broj redova matrice
     .word 2
 
-  columns:
+  columns:      # broj kolona matrice
     .word 2
 
 .text
   main:
-    # pocetna adresa matrice
-    la a0, matrix
-    # a1 = broj redova
-    lw a1, rows
-    # a2 = broj kolona
-    lw a2, columns
-    # i = 0
-    li a3, 0
-    # j = 0 
-    li a4, 0
-    # min(a5) = infinity
-    li a5, 0x7FFFFFFF
-    # max(a6) = -infinity
-    li a6, 0x80000000
+    
+    la a0, matrix   # ucitavanje pocetne adrese matrice u registar a0
+    
+    lw a1, rows     # ucitavanje broja redova u registar a1
+    
+    lw a2, columns  # ucitavanje broja kolona u registar a2
+   
+    li a3, 0        # inicijalizacija i = 0
+    
+    li a4, 0        # inicijalizacija j = 0
+    
+    li a5, 0x7FFFFFFF   # inicijalizacija min(a5) = beskonacno
+    
+    li a6, 0x80000000  # inicijalizacija max(a6) = -beskonacno
 
     #while i < rows
     whileOne:
-    # initialize j
-    li a4, 0
-      # while j<columns
+    
+    li a4, 0          # inicijalizacija j = 0
+      
       whileTwo:
-        #a7 = matrix[i][j]
-        lw a7, 0(a0)
-        blt a7, a5, setLow
-        bgt a7, a6, setHigh
-        addi a0, a0, 4
-        addi a4, a4, 1
-        bne a2, a4, whileTwo
-      addi a3, a3, 1
-      bne a1, a3, whileOne
-      #kraj loopa, ubacivanje najmanjeg u adresu najveceg i ubacivanje najveceg u adresu najmanjeg
-      sw a6, 0(s2)
-      sw a5, 0(s3)
-      j endProgram
+       
+        lw a7, 0(a0)  # ucitavanje vrednosti elementa matrice u registar a7
+        blt a7, a5, setLow   # ako je vrednost manja od min(a5), skok na oznacenu metodu
+        bgt a7, a6, setHigh  # ako je vrednost veca od max(a6), skok na oznacenu metodu
+        addi a0, a0, 4  # pomeranje na sledeci element matrice (4 bajta po elementu)
+        addi a4, a4, 1  # inkrementovanje j
+        bne a2, a4, whileTwo   # ako j < columns, skok na pocetak unutrasnje petlje
+      addi a3, a3, 1  # inkrementovanje i
+      bne a1, a3, whileOne   # ako i < rows, skok na pocetak spoljasnje petlje
+     
+      sw a6, 0(s2)    # cuvanje najveceg elementa matrice na adresi najmanjeg
+      sw a5, 0(s3)    # cuvanje najmanjeg elementa matrice na adresi najveceg
+      j endProgram    # skok na kraj programa
 
 
   setLow:
-    mv a5, a7
-    mv s2, a0
-    j whileTwo
+    mv a5, a7   # a5 postaje vrednost elementa matrice (min(a5))
+    mv s2, a0   # cuvanje adrese elementa sa min(a5)
+    j whileTwo  # skok na pocetak unutrasnje petlje
 
   setHigh:
-    mv s3, a0
-    mv a6, a7
-    j whileTwo
+    mv s3, a0   # cuvanje adrese elementa sa max(a6)
+    mv a6, a7   # a6 postaje vrednost elementa matrice (max(a6))
+    j whileTwo  # skok na pocetak unutrasnje petlje
 
   endProgram:
-    nop
+    nop  
