d624f3433068410b0c2839f9393b99fe7635264a
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2023-02-23 10:21:31 +0100

    final

--- anotherTask/project.asm
+++ anotherTask/project.asm
@@ -1,81 +1,47 @@
-INCLUDE 'EMU8086.INC'
 .MODEL SMALL
 .STACK 100h
-.DATA        
-    ARR DB 50 DUP(?)    ;declare array
-
+.DATA
+    array db 3, 8, 2, 5, 12, 16, 44, 2, 9, 100
+    
 .CODE
     MAIN PROC
+        MOV AX, @DATA
+        MOV DS, AX
         
-        MOV AX,@DATA
-        MOV DS,AX
-        
-        PRINT "Number of data(1-9): "
-        MOV AH,1
-        INT 21h
-        AND AL,0FH  ; convert ascii value       
-        
-        XOR CX,CX    ;to set zero
-        MOV CL,AL
-        PUSH CX
-        PUSH CX  
-        LEA SI,ARR
-        
-        PRINTN        ;print new line
-        PRINTN
-        PRINT "Enter Data: "    ;print message       
-     
-     INPUT:         
-         MOV AH,1
-         INT 21h
-        
-         MOV ARR[SI],AL
-         INC SI
-         LOOP INPUT           ;looping 
-         
-         POP CX          
-         LEA SI,ARR
-         ADD CX,SI
-        
-     XOR BX,BX      
-     SORT:            ;sorting label
-          SUB CX,1
-          LEA SI,ARR
-          CMP CX,SI
-          JLE EXIT
-          
-          SWAP:
-            CMP SI,CX
-            JGE SORT
-                        
-            MOV BL,ARR[SI]   ; bubble sort algorithm
-            MOV BH,ARR[SI+1]
-            CMP BL,BH
-            JLE INCREMENT
-            
-            MOV ARR[SI],BH
-            MOV ARR[SI+1],BL
-            INC SI
+        MOV CX, 0
+        MOV CL, 10
 
-            JMP SWAP  
-         
+        LEA SI, array
 
-      INCREMENT:
-           INC SI
-           JMP SWAP
-      EXIT:
-           LEA SI,ARR
-           POP CX
-           PRINTN
-           PRINTN
-           PRINT "After Sorting: "
-         
-      OUTPUT: 
-            MOV DL,ARR[SI]  ;show output
-            MOV AH,2
-            INT 21h
-            INC SI
-            LOOP OUTPUT
         
-     MAIN ENDP
-END MAIN
+        MOV BX, 0
+        
+     BUBBLE_SORT:
+        dec CX
+        LEA SI, array
+        CMP CX, SI
+        JLE EXIT
+        
+        SWAP:
+        CMP SI, CX
+        JGE BUBBLE_SORT
+        
+        MOV BL, array[SI]
+        MOV BH, array[SI+1]
+        CMP BL, BH
+        JLE INCREMENT
+        
+        MOV array[SI], BH
+        MOV array[SI+1], BL
+        INC SI
+        
+        JMP SWAP
+        
+        
+     INCREMENT:
+        INC SI
+        JMP SWAP
+     EXIT:
+     LEA SI, array
+
+     END
--- firstTask/project.s
+++ firstTask/project.s
@@ -1,77 +1,61 @@
 .data
-matrica: .word 2, 2, 3, 4, 1, 9, 7, 8, 3
-rowsize: .word 3 
-colsize: .word 3
+  matrix:
+    .word 1 2
+    .word 3 4
+
+  rows:
+    .word 2
+
+  columns:
+    .word 2
 
 .text
-main: 
+  main:
+    # pocetna adresa matrice
+    la a0, matrix
+    # a1 = broj redova
+    lw a1, rows
+    # a2 = broj kolona
+    lw a2, columns
+    # i = 0
+    li a3, 0
+    # j = 0 
+    li a4, 0
+    # min(a5) = infinity
+    li a5, 0x7FFFFFFF
+    # max(a6) = -infinity
+    li a6, 0x80000000
 
-la x10, matrica # adress of first element (0x10000000)
-add x11, x0, x10 # copy x10
-add x30, x0, x10 # min address
-add x31, x0, x10 # max address
+    #while i < rows
+    whileOne:
+    # initialize j
+    li a4, 0
+      # while j<columns
+      whileTwo:
+        #a7 = matrix[i][j]
+        lw a7, 0(a0)
+        blt a7, a5, setLow
+        bgt a7, a6, setHigh
+        addi a0, a0, 4
+        addi a4, a4, 1
+        bne a2, a4, whileTwo
+      addi a3, a3, 1
+      bne a1, a3, whileOne
+      #kraj loopa, ubacivanje najmanjeg u adresu najveceg i ubacivanje najveceg u adresu najmanjeg
+      sw a6, 0(s2)
+      sw a5, 0(s3)
+      j endProgram
 
-lw x12, rowsize # load n from it's address in memory (0x10000024) in case of a 9 element matrix
-lw x13, colsize # load m from it's address in memory (0x10000028) in case of a 9 element matrix
-mul x6, x12, x13 # x6 = n * m
 
-addi x7, x0, 1 # i = 1
-addi x8, x0, 1 # j = 1
+  setLow:
+    mv a5, a7
+    mv s2, a0
+    j whileTwo
 
-#matrix_loop:
- # bge x7, x6, end_matrix # end of matrix
-  ##sw x7, 0(x11) # store i into matrix
-  #sw x8, 0(x11)
-  #addi x8, x8, -2
-  #addi x7, x7, 1 # i++
-  #addi x11, x11, 4 # next element
-  #jal x0, matrix_loop
-#end_matrix:
-#  addi x7, x0, 0 # i = 0
-#  addi x11, x10, 0 # copy x10
+  setHigh:
+    mv s3, a0
+    mv a6, a7
+    j whileTwo
 
-loop_min:
-  bge x9, x6, end_min # end of matrix
-  lw x28, 0(x11) # load current element
-  lw x29, 0(x30) # load min element
-  bgt x29, x28, swap_min # if min > current then swap
-  jal x1, increment # jumps to function for altering i/j values
-  addi x11, x11, 4 # next element
-  jal x0, loop_min # loop
-swap_min:
-  addi x30, x11, 0 # update min
-  jal x0, loop_min # loop
-end_min:
-  addi x7, x0, 1 # i = 1
-  addi x8, x0, 1 # j = 1
-  addi x9, x0, 1 # x9 = 1
-  addi x11, x10, 0 # copy x10
-
-loop_max:
-  bge x9, x6, finish # end of matrix
-  lw x28, 0(x11) # load current element
-  lw x29, 0(x31) # load max element
-  blt x29, x28, swap_max # if max < current then swap
-  jal x1, increment # jumps to function for altering i/j values
-  addi x11, x11, 4 # next element
-  jal x0, loop_max # loop
-swap_max:
-  addi x31, x11, 0 # update max
-  jal x0, loop_max # loop
-
-increment:
-  mul x9, x7, x8 # multiplies i and j in order to check in the main loop if the current element is the last one
-  addi x7, x7, 1 # i++
-  bgt x7, x12, increase # checks if i has exceeded row size increase j and return i to 1
-  jalr x1, x1, 0 # return where you left off in the main loop
-increase:
-  addi x8, x8, 1 # increases j by 1, but there is no need to check if the new number has exceeded the column size because x9 has reached the value needed to stop the main loop
-  addi x7, x0, 1 # returns value of i to 1
-  jalr x1, x1, 0 # return where you left off in the main loop
-finish:
-  lw x28, 0(x30) # load numbers
-  lw x29, 0(x31)
-  sw x28, 0(x31) # swap numbers
-  sw x29, 0(x30)
-
-nop
+  endProgram:
+    nop
